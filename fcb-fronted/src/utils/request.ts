0025946bb778122d11918965f26d700d92daf074
lan <hhxk666@gmail.com>
2023-08-14 21:05:53 +0800

    add:fronted

--- fcb-fronted/src/utils/request.ts
+++ fcb-fronted/src/utils/request.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:12345",
+  baseURL: "/",
   timeout: 6000000,
   headers:{
     'Authorization':localStorage.getItem('auth')
--- fcb-fronted/vite.config.ts
+++ fcb-fronted/vite.config.ts
@@ -6,7 +6,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
