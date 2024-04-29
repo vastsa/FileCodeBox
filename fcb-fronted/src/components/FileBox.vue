9602ad3a125c6fe5a6ba836553f9c70358e3e208
lan <hhxk666@gmail.com>
2024-04-29 22:39:48 +0800

    fix: 文件二维码在前端拼接结果好像错误

--- fcb-fronted/src/components/FileBox.vue
+++ fcb-fronted/src/components/FileBox.vue
@@ -64,6 +64,9 @@ function renderMarkdown(message: string) {
 function getQrCodeUrl(code:string) {
   return code.startsWith('http') ? code : window.location.origin +  code;
 }
+function getShareQrCodeUrl(code:string) {
+  return window.location.origin + '/#/?code=' + code;
+}
 </script>
 
 <template>
@@ -122,7 +125,7 @@ function getQrCodeUrl(code:string) {
             <el-progress v-if="value.status!='success' && value.status!='fail'" striped :percentage="value.percentage" :text-inside="true"
                          :stroke-width="20"></el-progress>
             <div v-else style="display: flex;justify-content: space-between">
-              <qrcode-vue :value="getQrCodeUrl(value.code)" :size="100"></qrcode-vue>
+              <qrcode-vue :value="getShareQrCodeUrl(value.code)" :size="100"></qrcode-vue>
               <div style="display: flex;flex-direction: column;justify-content: space-around">
                 <el-tag size="large" style="cursor: pointer" @click="copyText(value.code)">{{ value.code }}</el-tag>
                 <el-tag size="large" type="success" style="cursor: pointer" @click="copyText(value.code,1);">{{ t('fileBox.copyLink') }}
