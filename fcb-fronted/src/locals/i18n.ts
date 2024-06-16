fe3bc5959fcfeaf97ba40041ff0e76a44124728e
lan <hhxk666@gmail.com>
2024-06-16 11:06:33 +0800

    fix:默认使用中文

--- fcb-fronted/src/locals/i18n.ts
+++ fcb-fronted/src/locals/i18n.ts
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n' //引入vue-i18n组件
 import messages from './index'
-const language = (
-  navigator.language  || 'zh_cn'
-).toLowerCase();
-const lang = (localStorage.getItem('language') || language).replace(/-/, '_');
-
+let language = (
+  navigator.language || 'zh_cn'
+).toLowerCase().replace(/-/, '_');
+if (['zh','zh_cn','en','es','zh_tw'].indexOf(language) === -1) {
+  language = 'zh_cn'
+}
+const lang = (localStorage.getItem('language') || language);
 const i18n = createI18n({
   silentTranslationWarn: true,
   globalInjection: true,
