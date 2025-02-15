799b49b81dd86609163601c12bb2710856855188
Lan <xzu@live.com>
2025-02-15 22:04:47 +0800

    feat: remove fronted 2023

--- fcb-fronted/src/stores/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia';
-import {ref} from "vue";
-
-export const useConfigStore = defineStore('config', () => {
-  const config:any = ref(JSON.parse(localStorage.getItem('config') || '{}') || {}); // 配置
-  return { config };
-});
