548d625827a887211d432b11dbb77f7b4c1a4585
Lan <xzu@live.com>
2025-03-02 14:52:01 +0800

    feat: #258

--- themes/2024/assets/DashboardView-CSAuWj6G.js
+++ themes/2024/assets/DashboardView-CSAuWj6G.js
@@ -1,4 +1,4 @@
-import{c as g,d as v,H as w,o as _,a as u,b as t,n as a,h as e,i as k,t as i,g as y,F as U,q as F,A as M,e as p,J as C,K as z}from"./index-Bnh8PeoH.js";import{F as m}from"./file-BdcXN_Bb.js";import{H as D,T as S}from"./trash-DIj4NP4N.js";/**
+import{c as g,d as v,H as w,o as _,a as u,b as t,n as a,h as e,i as k,t as i,g as y,F as U,q as F,A as M,e as p,J as C,K as z}from"./index-DdVtieIt.js";import{F as m}from"./file-BwG-MAXq.js";import{H as D,T as S}from"./trash-DUfP1Qlp.js";/**
  * @license lucide-vue-next v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
