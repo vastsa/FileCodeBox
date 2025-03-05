72190331b9ea8f99c26db99a98a55f1ea9604519
Lan <hhxk666@gmail.com>
2025-03-06 00:25:02 +0800

    fix: 前端去除/

--- themes/2024/assets/file-DnnQU8q_.js
+++ themes/2024/assets/file-DnnQU8q_.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B2REZq7f.js";/**
+import{c as a}from"./index-YD9XVcaL.js";/**
  * @license lucide-vue-next v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
