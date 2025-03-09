45be97a258602798b0adb8964bad801787ffda0f
Lan <xzu@live.com>
2025-03-09 22:46:17 +0800

    fix: #272 #283 #273

--- themes/2024/assets/hard-drive-DfARARSd.js
+++ themes/2024/assets/hard-drive-DfARARSd.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-Lw6tXblc.js";/**
+import{c as e}from"./index-DphCS8bB.js";/**
  * @license lucide-vue-next v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
