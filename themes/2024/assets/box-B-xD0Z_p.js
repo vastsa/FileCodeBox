c4304d3525f6d591d24c6adc050106c99ecac09e
Lan <hhxk666@gmail.com>
2025-03-12 22:46:25 +0800

    feat: 适配pwa

--- themes/2024/assets/box-B-xD0Z_p.js
+++ themes/2024/assets/box-B-xD0Z_p.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DphCS8bB.js";/**
+import{c as a}from"./index-BJNmHON7.js";/**
  * @license lucide-vue-next v0.445.0 - ISC
  *
  * This source code is licensed under the ISC license.
