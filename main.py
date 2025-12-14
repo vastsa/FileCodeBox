ae0577048e21b7febb80f587f8b8149c587e17c8
wkitt4 <60107497+wkitt4@users.noreply.github.com>
2025-12-15 00:35:36 +0800

    支持IPv6

--- main.py
+++ main.py
@@ -150,5 +150,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        app="main:app", host="0.0.0.0", port=settings.port, reload=False, workers=1
+        app="main:app", host="::", port=settings.port, reload=False, workers=1
     )
