8cdb9725835e23ba1df11f87950e10a24bf3bd0c
Lan <hhxk666@gmail.com>
2025-12-31 03:45:52 +0800

    fix: https://github.com/vastsa/FileCodeBox/issues/444

--- themes/2024/assets/trash-BCIzB-P6.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./index-Ya6bAvM4.js";const e=a("trash",[["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]]);export{e as T};
