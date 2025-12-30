287d9aeedbefcc1ac337dd5b8e26d7f1993a9b4c
Lan <hhxk666@gmail.com>
2025-12-31 03:24:25 +0800

    feat: 支持S3 预签名上传

---
+++ themes/2024/assets/file-CvarIEZS.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-PTyx6WsQ.js";const t=a("file",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}]]);export{t as F};
