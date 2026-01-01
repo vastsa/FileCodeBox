69972e28b348473436459c5ca055c64b2926cc0c
Lan <hhxk666@gmail.com>
2026-01-01 12:46:10 +0800

    fix: https://github.com/vastsa/FileCodeBox/issues/449

--- themes/2024/assets/box-C2OkK29z.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./index-DaP6hnmm.js";const e=a("box",[["path",{d:"M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z",key:"hh9hay"}],["path",{d:"m3.3 7 8.7 5 8.7-5",key:"g66t2b"}],["path",{d:"M12 22V12",key:"d0xqtd"}]]);export{e as B};
